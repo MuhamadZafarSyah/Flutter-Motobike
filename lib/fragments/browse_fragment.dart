80a6dea9872a5d34540968ef92f375628294f92e
Muhamad Zafar Syah <zafarsyah123@gmail.com>
2025-08-26 20:07:49 +0700

    Refactor BrowseFragment to use ListView; implement header with logo and notification icon

--- lib/fragments/browse_fragment.dart
+++ lib/fragments/browse_fragment.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BrowseFragment extends StatelessWidget {
   const BrowseFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Browse Fragment',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    return ListView(
+      padding: EdgeInsets.all(0),
+      children: [Gap(30 + MediaQuery.of(context).padding.top), buildHeader()],
+    );
+  }
+
+  Widget buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'assets/logo_text.png',
+            height: 38,
+            fit: BoxFit.fitHeight,
+          ),
+          Container(
+            height: 46,
+            width: 46,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            alignment: Alignment.center,
+            child: Image.asset(
+              'assets/ic_notification.png',
+              height: 24,
+              width: 24,
+            ),
+          ),
+        ],
       ),
     );
   }
